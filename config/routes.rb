ac19829a7c8f0e480120fb2b6a4417e5954abb56
mariomoinet <mariomoinet@outlook.fr>
2017-09-04 22:57:15 +0200

    modification profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
-  :path_names=>{:sign_in=>'login',:sign_out=>'logout',:edit=>'profile'}
+  :path_names=>{:sign_in=>'login',:sign_out=>'logout',:edit=>'profile'},
+  :controllers=>{:registrations=>'registrations'}
 
   root 'pages#home'
   get 'pages/home'
