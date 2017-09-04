e4dc792c32037d79de5a8339efc1bff6b63ba09c
mariomoinet <mariomoinet@outlook.fr>
2017-09-04 21:39:07 +0200

    page utilisateur et css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
   :path_names=>{:sign_in=>'login',:sign_out=>'logout',:edit=>'profile'}
-  
+
   root 'pages#home'
   get 'pages/home'
-
+resources :users, only: [ :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
