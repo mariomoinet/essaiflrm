538da5725bde684b25f8c237320ca47ec77cbfed
mariomoinet <mariomoinet@outlook.fr>
2017-09-05 22:27:10 +0200

    supp les photos

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'pages/home'
 resources :users, only: [ :show]
 resources :rooms
+resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
