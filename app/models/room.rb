89025b9f6df3373fb86aea5f9eb79bf15a23fa5f
mariomoinet <mariomoinet@outlook.fr>
2017-09-05 11:43:30 +0200

    ajout des models

---
+++ app/models/room.rb
@@ -0,0 +1,11 @@
+class Room < ApplicationRecord
+  belongs_to :user
+  validates :room_type, presence: true
+  validates :accommodate, presence: true
+  validates :bed_room, presence: true
+  validates :bath_room, presence: true
+  validates :listing_name, presence: true, length: {maximum: 75} # 75 caractères max
+  validates :summary, presence: true, length: {maximum: 600}
+  validates :address, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 5 } #seulement des nombres entiers supérieurs à 5
+end
